6c08cce47947802bb1c761bc49b38e1c3dad33a2
elqiashok <elqiashok25@gmail.com>
2021-06-09 13:12:59 +0700

    update mod10

--- login.php
+++ login.php
@@ -1,19 +1,28 @@
 <?php 
+
+// Tujuan kode ini adalah melakukan proses login user dengan memeriksa username dan passwordnya
+// kemudian membandingkannya dengan yang ada di database
+
+
+// Menyambungkan ke database
 include 'koneksi.php';
  
+
+// Mengambil data username pada form login akun yang sudah di isi oleh user melalui method POST
 $username = $_POST['username'];
+// Mengambil data password pada form login akun yang sudah di isi oleh user melalui method POST serta melakukan hash password
 $password = md5($_POST['password']);
  
+
+// Mencari data user pada table customer dengan menggunakan username dan password untuk melakukan validasi login
 $login = mysql_query("select * from customer where username='$username' and password='$password'");
-$cek = mysql_num_rows($login);
+$cek = mysql_num_rows($login); // $cek berguna untuk menghitung apakah user sudah berhasil login ke dalam sistem
  
-if($cek > 0){
-	session_start();
-	$_SESSION['username'] = $username;
-	$_SESSION['status'] = "login";
-	header("location:admin/index.php");
+if($cek > 0){ // Jika $cek bernilai lebih dari 0 maka
+	session_start(); // Untuk memulai session dengan menyimpan data user yang sedang login
+	$_SESSION['username'] = $username; // Melakukan perbaruan cookies pada username yang berhasil login
+	$_SESSION['status'] = "login"; // Melakukan perbaruan cookies pada status yang berhasil login
+	header("location:admin/index.php"); //Lalu mengarahkan user pada halaman index.php
 }else{
-	header("location:index.php");	
+	header("location:index.php");	// Apabila $cek tidak lebih dari 0 maka user akan diarahkan ke halaman index.php
 }
- 
-?>
