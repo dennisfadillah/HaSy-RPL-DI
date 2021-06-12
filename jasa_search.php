0b577e24e555044e8bc20ff7b8e9df6b66d39d49
dennisfadillah <dennisf@student.telkomuniversity.ac.id>
2021-06-12 13:14:00 +0700

    commit search script

---
+++ jasa_search.php
@@ -0,0 +1,41 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search Penyedia Jasa</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <input type="text" placeholder="Search" name="search">
+        <button type="submit" name="submit">Search</button>
+    </form>
+</body>
+
+</html>
+<?php
+require_once('db.php');
+
+if (isset($_POST['submit'])) {
+    $searchValue = $_POST['search'];
+    $conn = new Database();
+    $conn = $conn->getInstance();
+    if ($conn->connect_error) {
+        echo "connection Failed: " . $conn->connect_error;
+    } else {
+        $sql = "SELECT * FROM penyediajasa WHERE nama_lengkap LIKE '%$searchValue%'";
+
+        $result = $conn->query($sql);
+        while ($row = $result->fetch_assoc()) {
+            echo $row['nama'] . "<br>";
+            echo $row['harga'] . "<br>";
+        }
+
+
+    }
+}
+
+
+
+?>
